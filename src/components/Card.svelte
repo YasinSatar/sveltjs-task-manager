d8f600d983a63c507269f9c2ac0da0aad006013b
YasinSatar <34621981+YasinSatar@users.noreply.github.com>
2022-08-22 14:58:15 +0300

    Task Create,Delete,Completed and Animation

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,54 +1,92 @@
 <script>
-        import {members} from "../stores/store"
-export let task;
+    import { members, allTasks } from "../stores/store";
+    export let task;
+
+    const handleComplete = (id) =>{
+        allTasks.update(currentTasks =>{
+            let completedTask = $allTasks.find(task=>task.id ===id)
+            completedTask.isCompleted =!completedTask.isCompleted;
+            return currentTasks;
+        })
+    }
+    const handleRemove = (id) =>{
+        allTasks.update(currentTasks =>{
+            return currentTasks.filter(task=>task.id !==id)
+        })
+    }
 </script>
 
-<div class="card">
+<div
+    class="card"
+    class:completed={task.isCompleted}
+    on:dblclick={() => {
+        handleComplete(task.id);
+    }}
+>
+    <span class="btn-delete"  
+    on:click={() => {
+        handleRemove(task.id);
+    }}>X</span>
     <h5>{task.title}</h5>
     <p>{task.description}</p>
     <div class="members">
         {#each $members as member (member.id)}
-        {#each task.persons as person}
-        {#if member.id==person}
-        <span>{member.name} </span>
-        {/if}
-        {/each}
+            {#each task.persons as person}
+                {#if member.id == person}
+                    <span>{member.name} </span>
+                {/if}
+            {/each}
         {/each}
     </div>
     <p>{task.deadline}</p>
 </div>
 
 <style>
-    .card{
+    .card {
         flex-basis: 15%;
         padding: 10px;
         min-width: 200px;
         max-width: 300px;
         max-height: 200px;
-        margin-right: 10px;
-        background-color: azure;
+        margin-right: 1.5rem;
+        background-color: rgb(253, 247, 154);
         border-radius: 15px;
         box-shadow: 2px 2px rgb(198, 186, 186);
-        text-align:left;
+        text-align: left;
+        cursor:pointer;
+        user-select: none;
+        line-height: 10px;
     }
-    p,span{
+    p,
+    span {
         text-align: left;
         font-size: 10px;
     }
-    .members{
+    .members {
         display: flex;
         column-gap: 5px;
     }
-    .members span:not(:last-child)::after{  
-       content: ",";
-       margin-left:-2px;
+    .members span:not(:last-child)::after {
+        content: ",";
+        margin-left: -2px;
     }
     .completed{
-        
-        background-color: rgb(166, 135, 196);
+
+        background-color: #99f8b6;
     }
-    .delete-btn{
-        font-size: 16px;
-        cursor: pointer;
+    .completed :not(.btn-delete) {
+        text-decoration: line-through;
     }
+    .btn-delete{
+        margin-right:-20px;
+        margin-top:-20px;
+        float:right;
+        font-size: 14px;
+        padding:3px 10px;
+        background-color:white;
+        border-radius: 50%;
+        border:1px solid #EAEAEA;
+        line-height: 1.5rem;
+    }
+ 
 </style>
--- src/components/ShowAllTasks.svelte
+++ src/components/ShowAllTasks.svelte
@@ -1,33 +1,47 @@
 <script>
     import Card from "./Card.svelte";
-    import {taskTypes,allTasks,completedTasks,inCompletedTasks} from "../stores/store";
+    import {
+        taskTypes,
+        allTasks,
+        completedTasks,
+        inCompletedTasks,
+    } from "../stores/store";
+    import {fade, scale} from "svelte/transition"
+    import {flip} from "svelte/animate"
 
     // -1 = All Tasks, 0 = inCompletedTasks, 1 = completedTasks
     export let selectedTaskId;
- 
+
     let selectedTask;
-    function changeTaskType(selectedTaskId){
-     selectedTask= $taskTypes.find(task => task.id===selectedTaskId).task;
-     return $selectedTask;
+    function changeTaskType(selectedTaskId) {
+        selectedTask = $taskTypes.find(
+            (task) => task.id === selectedTaskId
+        ).task;
+        return $selectedTask;
     }
-	
 </script>
 
 <div class="all-tasks">
-    {#if selectedTaskId==-1}
-    {#each $allTasks as task (task.id)}
-    <Card {task} />
-    {/each}
+    {#if selectedTaskId == -1}
+        {#each $allTasks as task (task.id)}
+            <div in:fade out:scale|local animate:flip={{duration:500}}>
+                <Card {task} />
+            </div>
+        {/each}
     {/if}
-    {#if selectedTaskId==1}
-    {#each $completedTasks as task (task.id)}
-    <Card {task} />
-    {/each}
+    {#if selectedTaskId == 1}
+        {#each $completedTasks as task (task.id)}
+        <div in:fade out:scale|local animate:flip={{duration:500}}>
+            <Card {task} />
+        </div>
+        {/each}
     {/if}
-    {#if selectedTaskId==0}
-    {#each $inCompletedTasks as task (task.id)}
-    <Card {task} />
-    {/each}
+    {#if selectedTaskId == 0}
+        {#each $inCompletedTasks as task (task.id)}
+        <div in:fade out:scale|local animate:flip={{duration:500}}>
+            <Card {task} />
+        </div>
+        {/each}
     {/if}
     <!-- {#each changeTaskType(selectedTaskId) as task (task.id)}
         <Card {task} />
@@ -41,7 +55,7 @@
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        row-gap: 1rem;
+        row-gap: 1.5rem;
     }
     .box {
         display: flex;
